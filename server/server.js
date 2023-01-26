b315bd6f597694c243e734cce140fa04414c09e1
Maurice Brown <mauricebrwn6890@gmail.com>
2023-01-26 15:48:21 -0600

    server boilerplate set up

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const axios = require('axios');
 
 const app = express();
+const giphy_api_key = process.env.GIPHY_API_KEY;
 // App PORT set with production check
 const PORT = process.env.PORT || 5000;
+require('dotenv').config()
 
 // Route includes
 const favoriteRouter = require('./routes/favorite.router');
@@ -15,6 +18,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Serve static files
 app.use(express.static('build'));
+
+app.get('/gifs', (req, res) => {
+  // Here is where we will ask the Giphy API
+  // for data:
+  axios({
+    method: 'GET',
+    url: `https://api.giphy.com/v1/gifs/trending?api_key=${giphy_api_key}`
+  }).then((response) => {
+    res.send(response.data);
+  }).catch((error) => {
+    console.log('GET /gifs fail:', error);
+    res.sendStatus(500);
+  })
+})
 
 // Routes
 app.use('/api/favorite', favoriteRouter);
