6dc281cc70bfd767e036450c3673596773b30be5
Maurice Brown <mauricebrwn6890@gmail.com>
2023-01-26 16:28:50 -0600

    git and post route router complete

--- server/routes/favorite.router.js
+++ server/routes/favorite.router.js
@@ -1,16 +1,39 @@
 const express = require('express');
+const { CommandCompleteMessage } = require('pg-protocol/dist/messages');
 const pool = require('../modules/pool');
 
 const router = express.Router();
 
 // return all favorite images
 router.get('/', (req, res) => {
-  res.sendStatus(200);
+  const queryText = 'SELECT id,name FROM favorites';
+  pool.query(queryText)
+  .then((result) => {res.send(result.rows); })
+  res.sendStatus(200)
+  .catch((err) => {
+    CommandCompleteMessage.log('Error in SELECT favorite query', err);
+    res.sendStatus(500);
+  });
 });
 
 // add a new favorite
 router.post('/', (req, res) => {
-  res.sendStatus(200);
+  const newFavorite = req.body;
+  const queryText = `
+  INSERT INTO favorites
+  ("name")
+  VALUES
+  ($1)
+  `;
+  const queryValues=[
+    newFavorite.name
+  ];
+  pool.query(queryText,queryValues)
+  .then(() => { res.sendStatus(201);})
+  .catch((err) => {
+    console.log('Error completing SELECT favorite query', err);
+    res.sendStatus(500);
+  });
 });
 
 // update given favorite with a category id
