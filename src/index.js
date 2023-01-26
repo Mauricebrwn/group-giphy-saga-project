dbda27a88e2115cd219fbd7b1582ac9ffd6bcf03
Maurice Brown <mauricebrwn6890@gmail.com>
2023-01-26 15:36:23 -0600

    index boilerplate setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+import axios from 'axios';
+// Import the Sagas stuff:
+import createSagaMiddleware from 'redux-saga';
+import { takeEvery, put } from 'redux-saga/effects';
 import App from './components/App/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+function* rootSaga() {
+    yield takeEvery('???', unKnown)
+}
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+    combineReducers({ unKnown }),
+    applyMiddleware(logger, sagaMiddleware)
+  );
+
+sagaMiddleware.run(rootSaga);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('react-root'));
