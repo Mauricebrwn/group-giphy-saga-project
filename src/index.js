59e2b26c9d2a6c9bb4dd68c39116a6738a05c452
Shong Lee <Shonglee17@gmail.com>
2023-01-27 09:43:36 -0600

    fixed boiler plate

--- src/index.js
+++ src/index.js
@@ -95,4 +95,13 @@ const store = createStore(
 
 sagaMiddleware.run(rootSaga);
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('react-root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
